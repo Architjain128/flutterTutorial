b82296a3b7d692aad857b0924a3e0056950c7667
FlutLab.io <>
2022-07-18 19:30:58 +0000

    Autosave

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 const APPNAME = "Flutter Tutorial";
 // const HomePageTitle = "Home Page";
 const HomePageTitle = "New Home Page";
-const MaterialColor PRIMARY_THEME = Colors.blue;
-// const MaterialColor PRIMARY_THEME = Colors.purple;
+// const MaterialColor PRIMARY_THEME = Colors.blue;
+const MaterialColor PRIMARY_THEME = Colors.purple;
 const MaterialColor SECONDARY_THEME = Colors.purple;
